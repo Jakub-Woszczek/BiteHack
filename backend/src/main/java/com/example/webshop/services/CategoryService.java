382b224cec6c2af96fe452a6d6569e8ba980940d
Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>
2026-01-10 23:04:20 +0100

    Categories and review services (#9)
    * category_service
    
    * category_service
    
    * category_service
    
    ---------
    
    Co-authored-by: Lynxman21 <mateuszrys97@gmail.com>

--- backend/src/main/java/com/example/webshop/services/CategoryService.java
+++ backend/src/main/java/com/example/webshop/services/CategoryService.java
@@ -1,6 +1,15 @@
 package com.example.webshop.services;
 
+import com.example.webshop.entities.Category;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class CategoryService {
     @PersistenceContext
     EntityManager entityManager;
@@ -20,12 +29,24 @@ public class CategoryService {
         return categories.getFirst();
     }
 
-    public boolean categoryExists(String name) {
+    private boolean categoryExists(String name) {
         List<Category> categories = entityManager.createQuery("select c from Categories c where c.name=:name", Category.class)
                 .setParameter("name",name)
                 .getResultList();
 
         return !categories.isEmpty();
+    }
+
+    public Category getCategoryIfExists(String name) {
+        if (categoryExists(name)) {
+            List<Category> categories = entityManager.createQuery("select c from Categories c where c.name=:name", Category.class)
+                    .setParameter("name",name)
+                    .getResultList();
+
+            return categories.getFirst();
+        }
+
+        return null;
     }
 
     @Transactional
